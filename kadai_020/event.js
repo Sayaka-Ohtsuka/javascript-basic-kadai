ad5cf6f07e371efb038151d43bbdd396dc23b97f
Sayaka-Ohtsuka <hoshinokoyozora@gmail.com>
2025-07-30 20:59:00 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,24 +1,10 @@
+const btn = document.getElementById("btn");
 
-const btn = document.getElementById('btn');
+const text = document.getElementById("text");
 
-const parentList = document.getElementById('text');
-
-
-btn.addEventListener('click', () => {
-    
-
-  // li要素を新しく作成する
-  const childList = document.createElement('text');
-
-  childList.textContent = 'ボタンをクリックしました';
-
-  
-  parentList.appendChild(childList);
-});
-
-btn.addEventListener('click', () => {
-    
-  // parentListの最初の子要素を削除する
-  parentList.removeChild(parentList.firstChild);
-
-});
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener("click", () => {
+  // textに「ボタンをクリックしました」というテキストを追加する
+  text.textContent = "ボタンをクリックしました";
+  console.log(text);
+})
